70c4272ccdea35f06946bb75e41fe5eccc618686
Allan Crooks <allan.crooks@sixtyten.org>
2017-04-23 14:16:07 +0100

    tab-to-space conversion.

--- setup.py
+++ setup.py
@@ -8,48 +8,48 @@ import io
 import setuptools
 
 with io.open('README.rst', encoding='utf-8') as readme:
-	long_description = readme.read()
+    long_description = readme.read()
 
 name = 'skeleton'
 description = ''
 
 params = dict(
-	name=name,
-	use_scm_version=True,
-	author="Allan Crooks",
-	author_email="allan@increment.one",
-	description=description or name,
-	long_description=long_description,
-	url="https://github.com/the-allanc/" + name,
-	packages=setuptools.find_packages(),
-	include_package_data=True,
-	namespace_packages=name.split('.')[:-1],
-	python_requires='>=2.7',
-	install_requires=[
-	],
-	extras_require={
-		'testing': [
-			'pytest>=2.8',
-			'pytest-sugar',
-		],
-		'docs': [
-			'sphinx',
-			'jaraco.packaging>=3.2',
-			'rst.linker>=1.9',
-		],
-	},
-	setup_requires=[
-		'setuptools_scm>=1.15.0',
-	],
-	classifiers=[
-		"Development Status :: 5 - Production/Stable",
-		"Intended Audience :: Developers",
-		"License :: OSI Approved :: MIT License",
-		"Programming Language :: Python :: 2.7",
-		"Programming Language :: Python :: 3",
-	],
-	entry_points={
-	},
+    name=name,
+    use_scm_version=True,
+    author="Allan Crooks",
+    author_email="allan@increment.one",
+    description=description or name,
+    long_description=long_description,
+    url="https://github.com/the-allanc/" + name,
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    namespace_packages=name.split('.')[:-1],
+    python_requires='>=2.7',
+    install_requires=[
+    ],
+    extras_require={
+        'testing': [
+            'pytest>=2.8',
+            'pytest-sugar',
+        ],
+        'docs': [
+            'sphinx',
+            'jaraco.packaging>=3.2',
+            'rst.linker>=1.9',
+        ],
+    },
+    setup_requires=[
+        'setuptools_scm>=1.15.0',
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+    ],
+    entry_points={
+    },
 )
 if __name__ == '__main__':
-	setuptools.setup(**params)
+    setuptools.setup(**params)
