fcd195409b6bd60333530b401909736b3315db69
Allan Crooks <allan.crooks@sixtyten.org>
2017-04-25 13:24:43 +0100

    Automatically generate copyright based on author in setup and current year.

--- docs/conf_as_extension.py
+++ docs/conf_as_extension.py
@@ -4,5 +4,9 @@ def setup(app):
     app.connect('builder-inited', init)
     
 def init(app):
-    pass
+    # I prefer the copyright to include the date.
+    if app.config.author == app.config.copyright:
+        from datetime import date
+        copyright = '{} {}'.format(date.today().year, app.config.author)
+        app.config.copyright = copyright
 
