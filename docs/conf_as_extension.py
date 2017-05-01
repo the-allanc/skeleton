faa7e463c66dbe53ed4422ec70fd36aa0142fdd7
Allan Crooks <allan@increment.one>
2017-05-01 22:41:18 +0100

    Include setup page which populates the project name and repository.

--- docs/conf_as_extension.py
+++ docs/conf_as_extension.py
@@ -2,6 +2,11 @@ from __future__ import unicode_literals
 
 def setup(app):
     app.connect('builder-inited', init)
+
+prolog = '''
+.. |projname| replace:: {0.project}
+.. |repo| replace:: {0.package_url}
+'''
     
 def init(app):
     # I prefer the copyright to include the date.
@@ -9,4 +14,4 @@ def init(app):
         from datetime import date
         copyright = '{} {}'.format(date.today().year, app.config.author)
         app.config.copyright = copyright
-
+    app.config.rst_epilog = prolog.format(app.config)
--- docs/index.rst
+++ docs/index.rst
@@ -5,6 +5,7 @@
    :maxdepth: 1
 
    history
+   setup
 
 
 .. automodule:: skeleton
