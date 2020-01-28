65ed70f001996993c7c4156c5b5c7f70e7d2b3b3
Allan Crooks <allan@sixtyten.org>
2020-01-28 16:19:50 +0000

    Add attrmap related utilities.

--- crookbook.py
+++ crookbook.py
@@ -83,3 +83,146 @@ def described(inner):
         return six.python_2_unicode_compatible(cls)
 
     return described_maker
+
+
+class AttrItems(object):
+
+    def __getitem__(self, key):
+        try:
+            return getattr(self, key)
+        except AttributeError:
+            raise KeyError(key)
+        except TypeError:
+            if isinstance(key, int):
+                raise KeyError(key)
+            raise
+
+    def __setitem__(self, key, value):
+        try:
+            setattr(self, key, value)
+        except AttributeError:
+            raise KeyError(key)
+
+    def __delitem__(self, key):
+        try:
+            delattr(self, key)
+        except AttributeError:
+            raise KeyError(key)
+
+
+class ItemAttrs(object):
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        try:
+            self[name] = value
+        except KeyError:
+            raise AttributeError(name)
+
+    def __delattr__(self, name):
+        try:
+            del self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+
+class MappingNS(ItemAttrs):
+
+    def __repr__(self):
+        keys = sorted(self)
+        items = ("{}={!r}".format(k, self[k]) for k in keys)
+        return "{}({})".format(type(self).__name__, ", ".join(items))
+
+    def __str__(self):
+        return repr(self)
+
+    def __dir__(self):
+        superdir = getattr(super(MappingNS, self), '__dir__', None)
+        if superdir is not None:
+            sdir = superdir()
+        else:
+            sdir = dir(type(self)) + list(getattr(self, '__dict__', {}))
+        return sdir + list(self)
+
+
+try:
+    from types import SimpleNamespace as Namespace
+except ImportError:
+    from argparse import Namespace
+
+
+class attrdict(ItemAttrs, dict):
+    pass
+
+
+class nsdict(MappingNS, dict):
+    pass
+
+
+class nsmap(Namespace, AttrItems):
+    pass
+
+
+del Namespace
+
+
+def deep_remap(struct, newtype, maptypes=(dict,), seqtypes=(tuple, list)):
+    if isinstance(struct, maptypes):
+        res = newtype()
+        for k, v in struct.items():
+            res[k] = deep_remap(v, newtype, maptypes, seqtypes)
+        return res
+
+    if isinstance(struct, seqtypes):
+        return type(struct)(deep_remap(x, newtype, maptypes, seqtypes) for x in struct)
+
+    return struct
+
+
+def attrcopy(source, attrs_to_get, maptype=nsdict):
+    res = maptype()
+    for attrspec in reversed(sorted(attrs_to_get, key=len)):
+        _attrcopy(res, source, attrspec.split('.'), maptype)
+    return res
+
+
+def _attrcopy(res, source, attrspec, maptype=dict):
+    thisattr = attrspec[0]
+
+    # Just copy this attribute.
+    if len(attrspec) == 1:
+        # If it's already known to us, then don't set it.
+        # This probably implies its a namespace with attributes inside.
+        if thisattr not in res:
+            res[thisattr] = getattr(source, thisattr)
+        return
+
+    # Work out if are going to be dealing with a sequence.
+    innertype = None
+    if thisattr[-2:] in ['[]']:
+        thisattr, innertype = thisattr[:-2], thisattr[-2:]
+
+    # Navigate down into the next object.
+    value = getattr(source, thisattr)
+    attrspec = attrspec[1:]
+
+    # Prepare the container object we're going to put writing into.
+    if thisattr not in res:
+        if innertype == '[]':
+            res[thisattr] = type(value)(maptype() for _ in value)
+        else:
+            res[thisattr] = maptype()
+
+    newres = res[thisattr]
+
+    # Recurse our way downwards.
+    if innertype == '[]':
+        for i, (nres, s) in enumerate(zip(newres, value)):
+            _attrcopy(nres, s, attrspec, maptype)
+    else:
+        _attrcopy(newres, value, attrspec, maptype)
